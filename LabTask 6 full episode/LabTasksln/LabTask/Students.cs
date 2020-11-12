49a8b8aa762b935a5532d517afab2507d3284994
RaihanRakib <72722718+RaihanRakib@users.noreply.github.com>
2020-11-12 22:34:04 +0600

    Add files via upload

---
+++ LabTask 6 full episode/LabTasksln/LabTask/Students.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LabTask
+{
+    class Students
+    {
+ 
+        private
+        String name;
+        String id;
+        String department;
+        float cgpa;
+        private Students()
+        { }
+        public Students(String n, String i, String d, float cg)
+        {
+            name = n;
+            id = i;
+            department = d;
+            cgpa = cg;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine(" Student name is: " + name);
+            Console.WriteLine(" Student ID is: " + id);
+            Console.WriteLine(" Student Department is: " + department);
+            Console.WriteLine(" Student CGPA is: " + cgpa);
+        }
+    }
+}
